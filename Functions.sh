6d23c453f5e51aa4f91ce70b1ebfa53729685afe
chrism25 <chrism5825@gmail.com>
2022-02-15 21:52:12 +0000

    login

--- Functions.sh
+++ Functions.sh
@@ -1,1 +1,19 @@
 
+#!/bin/bash
+
+#Script Name: Functions
+#Purpose:List the login of the computer's users
+#Example usage: To know who uses the computer
+#Expected output: list of names of usersls
+#Variable: people
+#Function: users
+
+users () {
+
+    people=$(last)
+
+    echo $people
+
+}
+
+users
